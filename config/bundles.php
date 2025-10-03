3e39550c902926a0f4091ab3393dfb930db6ca74
Marijn Doeve <marijn@doeve.me>
2025-10-03 22:37:45 +0200

    Remove EasyAdmin integration
    - Removed EasyAdmin controllers and configurations.
    - Uninstalled `easycorp/easyadmin-bundle` and related dependencies.
    - Cleaned up all associated routes, bundles, and vendor references.

--- config/bundles.php
+++ config/bundles.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle;
 use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
-use EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle;
 use Sentry\SentryBundle\SentryBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\MakerBundle\MakerBundle;
@@ -14,7 +13,6 @@ use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
 use Symfony\UX\StimulusBundle\StimulusBundle;
 use Symfony\UX\Turbo\TurboBundle;
-use Symfony\UX\TwigComponent\TwigComponentBundle;
 use SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle;
 use Symfonycasts\SassBundle\SymfonycastsSassBundle;
 use Twig\Extra\TwigExtraBundle\TwigExtraBundle;
@@ -28,8 +26,6 @@ return [
     SecurityBundle::class => ['all' => true],
     WebProfilerBundle::class => ['dev' => true, 'test' => true],
     TwigExtraBundle::class => ['all' => true],
-    TwigComponentBundle::class => ['all' => true],
-    EasyAdminBundle::class => ['all' => true],
     DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SentryBundle::class => ['prod' => true],
